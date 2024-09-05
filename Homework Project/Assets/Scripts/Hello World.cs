37343813cd87203e4623f5b635728729e72f66ac
Wushutwist <aidenxmathews@gmail.com>
2024-09-05 08:48:43 -0400

    Update Hello World.cs

--- Homework Project/Assets/Scripts/Hello World.cs
+++ Homework Project/Assets/Scripts/Hello World.cs
@@ -7,7 +7,15 @@ public class HelloWorld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World"); 
+        Debug.Log("Hello World");
+        Debug.LogWarning("Hello, I am a warning");
+        Debug.LogError("Hello, I am an error");
+        //Author: Aiden Mathews//
+        /*
+         * I wish to make a 2D game
+         * I would want to make it turn based
+         * With a focus on crafting
+         */
     }
 
     // Update is called once per frame
